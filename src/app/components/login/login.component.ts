702cd437750c3a3acf06555fdc89832a33efcc84
MuhammadElshowehy <Muhammadgamalelshowehy@gmail.com>
2022-07-25 22:19:59 +0200

    one

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+    });
+  }
+
+  ngOnInit(): void { }
+
+  handelFormSubmit() {
+    console.log(this.loginForm);
+  }
+}
+
