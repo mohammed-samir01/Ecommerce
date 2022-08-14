d45583260d65c60d26e62afa2cf5bf24dd8732e9
mohamed_samir <86778943+mohammed-samir01@users.noreply.github.com>
2022-08-14 10:55:34 +0200

    finish cart , wishlist and order Api

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,34 +2,50 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'username' => $this->faker->unique()->userName,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'mobile' => $this->faker->unique()->phoneNumber,
+            'password' => bcrypt('123123123'),
+            'status' => 1,
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->attachRole(3);
+        });
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return static
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
     public function unverified()
     {
